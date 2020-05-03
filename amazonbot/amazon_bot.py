5539f13e3efc209df94dbf570dbd0cecf6c18bdf
shreygupta04 <shreygupta04@gmail.com>
2020-05-03 19:31:49 -0400

    cleaning up class file

--- amazonbot/amazon_bot.py
+++ amazonbot/amazon_bot.py
@@ -1,28 +1,6 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup as bs
 import os
-
-# search = input().replace(' ', '+')
-
-# headers = {
-#     'authority': 'www.amazon.com',
-#     'pragma': 'no-cache',
-#     'cache-control': 'no-cache',
-#     'dnt': '1',
-#     'upgrade-insecure-requests': '1',
-#     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.92 Safari/537.36',
-#     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-#     'sec-fetch-site': 'none',
-#     'sec-fetch-mode': 'navigate',
-#     'sec-fetch-dest': 'document',
-#     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-# }
-# response = requests.get('https://www.amazon.com/s?k={}&ref=nb_sb_noss_2'.format(search), headers=headers, verify=False)
-# soup_file = bs(response.text, 'html.parser')
-#
-# containers = soup_file.findAll("div", {"class": "sg-col-inner"})
-# print(containers)
 
 
 class AmazonProductAvailability:
